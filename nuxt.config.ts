8956999fd691043f7b261f509dc5744bb346ee56
Gunjan Patel <gunjan@hobbii.dk>
2023-03-28 13:38:04 +0200

    Some fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-
-  ],
+  target: "static",
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
     highlight: {
       theme: "monokai",
       preload: ['diff', 'json', 'js', 'ts', 'css', 'shell', 'html', 'md', 'yaml', 'php']
     }
-  }
+  },
 })
