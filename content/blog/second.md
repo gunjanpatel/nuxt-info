bab5d0829a50b07675c63088b98d67721818ea4b
Gunjan Patel <gunjan@hobbii.dk>
2023-04-02 00:04:31 +0200

    Remove next and previous links

--- content/blog/second.md
+++ content/blog/second.md
@@ -1,5 +1,5 @@
 ---
-title: My second Blog Post
+title: Second
 description: Second blog post
 imageUrl: https://images.unsplash.com/photo-1679673177212-8a011a4f86f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80
 alt: second blog post
@@ -7,7 +7,7 @@ author:
   name: Gunjan
   bio: All about Gunjan
   imageUrl: https://gravatar.com/avatar/a71d5c71e4c2236145ec0b8058bf9508?s=64
-date: 28th March 2023
+date: 2th March 2023
 layout: blog
 tags:
   - Frontend
--- layouts/blog.vue
+++ layouts/blog.vue
@@ -3,7 +3,6 @@
       <div class="grid grid-cols-4 items-start">
         <div class="col-span-4 md:col-span-3 lg:col-span-3 lg:pr-8">
           <slot />
-          <PrevNext :prev="prev" :next="next" />
         </div>
         <div class="mt-10 md:mt-0 lg:mt-0 col-span-4 md:col-span-1 lg:col-span-1 xl:col-span-1 2xl:col-span-1 lg:pl-10 lg:border-l-2 lg:border-gray-800">
           <Sidebar/>
@@ -13,8 +12,6 @@
 </template>
 <script setup>
 import Default from "~/layouts/default.vue";
-
 const { page } = useContent()
-const { next, prev } = useContent()
 </script>
 
