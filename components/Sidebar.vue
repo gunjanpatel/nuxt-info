7d60bc45d3eaa1b0a0bcc4ab711dfd49f50bc14c
Gunjan Patel <gunjan@hobbii.dk>
2023-04-02 03:28:12 +0200

    Improvements

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,22 +1,21 @@
 <template>
-  <TheTitle>My Blogs</TheTitle>
-  <ul class="list-none">
-    <li v-for="post in posts" class="my-2 py-5 border-b border-gray-200">
-      <a :href="nuxtApp.$getUrl(post._path)">{{ post.title }}</a>
-    </li>
-  </ul>
+  <div v-if="posts">
+    <TheTitle>Related Articles</TheTitle>
+    <ul class="list-none">
+      <li v-for="post in posts" class="my-2 py-5 border-b border-gray-200">
+        <a :href="nuxtApp.$getUrl(post._path)">{{ post.title }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script setup>
-const { page } = useContent()
+const path = useRoute();
 const nuxtApp = useNuxtApp()
 const {data: posts} = await useAsyncData(
   'posts-list',
-  () => queryContent('/blog')
-    .where({layout: {$eq: "blog"}})
+  (path) => queryContent('/blog')
+    .where({layout: {$eq: "blog"}, _path: {$ne: path}})
     .sort({date: 1})
     .find()
-
 )
-
-console.log(posts)
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
-    <main class="shadow-lg shadow-slate-800 mb-4 h-full" :id="'page-' + page._file.replace('.', '-')">
+    <main class="mb-4 h-full flex flex-col min-h-screen" :id="'page-' + page._file.replace('.', '-')">
       <Navbar/>
-      <div class="mx-auto gap-x-8 gap-y-16 lg:max-w-7xl px-4 sm:px-6 lg:px-8 py-20 sm:py-20 xl:col-span-3 2xl:col-span-3">
+      <div class="flex-1 overflow-y-auto mx-auto gap-x-8 gap-y-16 lg:max-w-7xl px-4 sm:px-6 lg:px-8 py-20 sm:py-20 xl:col-span-3 2xl:col-span-3">
           <TheTitle v-if="page && page.show_heading">{{ page.title }}</TheTitle>
           <slot />
       </div>
+      <div class="shadow-lg shadow-slate-800 mb-4">&nbsp;</div>
+      <Footer />
     </main>
-    <Footer />
 </template>
 <script setup>
 const { page } = useContent()
