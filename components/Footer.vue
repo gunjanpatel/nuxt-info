f877a6df88befad0986534242a3e22824cbb0f56
Gunjan Patel <gunjan@hobbii.dk>
2023-04-02 03:08:54 +0200

    Adding bottom

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,40 @@
 <template>
-  <footer class="min-h-[15em]">
-    <p class="w-full text-center text-gray-700 p-2 fixed bottom-10">
-      वसुधैव कुटुम्बकम् (Vasudhaiva Kutumbakam) (the world is one family)
-    </p>
-    <Copyright/>
+  <footer class="py-6">
+    <div class="container mx-auto px-4">
+      <div class="flex justify-between items-center">
+        <p class="text-gray-400">&copy; {{ new Date().getFullYear() }} GunjanPatel.info</p>
+        <div class="text-gray-400 text-center">
+          <p>वसुधैव कुटुम्बकम्</p>
+          <p class="text-xs italic">(Vasudhaiva Kutumbakam)</p>
+          <p class="text-xs italic">(The world is one family)</p>
+        </div>
+        <div class="flex items-center">
+          <a href="https://www.linkedin.com/in/jgunjanpatel/" target="_blank"
+             class="ml-6 text-gray-400 hover:text-gray-500">
+            <span class="sr-only">LinkedIn</span>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M4 4v16h16V4H4zm4.5 12.5h-2v-6h2v6zm-1-7.13c-.74 0-1.34-.6-1.34-1.33s.6-1.34 1.34-1.34c.74 0 1.34.6 1.34 1.34 0 .73-.6 1.33-1.34 1.33zm9.17 7.13h-2.01v-3.07c0-.72-.01-1.65-1-1.65-.99 0-1.15.77-1.15 1.57v3.15h-2v-6h1.97v.83h.03c.26-.5.89-1.03 1.95-1.03 2.09 0 2.48 1.38 2.48 3.18v3.02zM16.5 9.5c-.74 0-1.34-.6-1.34-1.33s.6-1.34 1.34-1.34c.73 0 1.33.6 1.33 1.34 0 .73-.6 1.33-1.33 1.33z"/>
+            </svg>
+          </a>
+          <a href="https://twitter.com/ergunjanpatel" target="_blank" class="ml-6 text-gray-400 hover:text-gray-500">
+            <span class="sr-only">Twitter</span>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
+            </svg>
+          </a>
+          <a href="https://github.com/gunjanpatel" target="_blank" class="ml-6 text-gray-400 hover:text-gray-500">
+            <span class="sr-only">GitHub</span>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clip-rule="evenodd"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
   </footer>
+
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="shadow-lg mb-4 h-full" :id="'page-' + page._file.replace('.', '-')">
+    <main class="shadow-lg shadow-slate-800 mb-4 h-full" :id="'page-' + page._file.replace('.', '-')">
       <Navbar/>
       <div class="mx-auto gap-x-8 gap-y-16 lg:max-w-7xl px-4 sm:px-6 lg:px-8 py-20 sm:py-20 xl:col-span-3 2xl:col-span-3">
           <TheTitle v-if="page && page.show_heading">{{ page.title }}</TheTitle>
